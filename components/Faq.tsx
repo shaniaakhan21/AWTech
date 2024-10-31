d04ef6438f7d8806eb676dd0792a833416b49db8
Shaniya Khan <shaniaakhan21@gmail.com>
2024-10-31 19:35:14 +0400

    Version 1.0

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -40,7 +40,7 @@ const Faq: React.FC = () => {
                 <div className='w-4/12 px-10 pt-96 pb-4 bg-cover bg-center rounded-2xl' style={{ backgroundImage: `url(${backgroundImage.src})` }}>
                     <div className='bg-[#222222] p-4 rounded-2xl shadow-xl'>
                         <h1 className='text-white text-2xl pb-2'>Still You Have Doubts ?</h1>
-                        <h5 className='text-[#f9f9f99c] font-extralight text-xs pb-4'>Tellus in hac habitasse platea dictumst</h5>
+                        <h5 className='text-[#f9f9f99c] font-extralight text-xs pb-4'>Contact us on whatsapp directly</h5>
                         <Button className="capitalize text-sm rounded-full mr-4 w-fit px-8 py-2 bg-[#FFC527] font-semibold text-[#222222] shadow-2xl " onClick={() => window.open('https://wa.me/971557736634', '_blank')}>
                             Submit Your Query
                         </Button>
