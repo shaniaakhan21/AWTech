615fe4dbe1410af80ccc514873552b1545cd674d
Shaniya Khan <shaniaakhan21@gmail.com>
2024-10-31 14:22:00 +0400

    About respinsiveness DONE

--- components/About.tsx
+++ components/About.tsx
@@ -11,26 +11,26 @@ import PhoneSVG from '../assests/images/servicesCards/phone.svg';
 
 const About: React.FC = () => {
     return (
-        <div className='px-32 pt-20 pb-4'>
+        <div className='px-2 pb-2 pt-4 sm:px-32 sm:pt-20 sm:pb-4'>
             <div>
                 <div>
-                    <h2 className='text-[#434343] text-sm'>24/7 hassle-free</h2>
+                    <h2 className='text-[#434343] text-sm text-center sm:text-start'>24/7 hassle-free</h2>
                 </div>
-                <div className='flex flex-row mt-2'>
-                    <div className='w-1/2'>
-                        <h1 className='text-[#222222] text-5xl font-bold mb-2 border-r border-[#222222]'>Repair And Maintenance Service Company</h1>
+                <div className='flex flex-col sm:flex-row mt-2'>
+                    <div className='w-full sm:w-1/2'>
+                        <h1 className='text-center sm:text-start text-[#222222] text-2xl sm:text-5xl font-bold mb-2 sm:border-r sm:border-[#222222]'>Repair And Maintenance Service Company</h1>
                     </div>
-                    <div className='w-1/2'>
-                        <h3 className='text-[#434343] text-lg ml-8'>Backed by impeccable experience and absolute customer satisfaction, we provide all-inclusive building maintenance and renovation services in Dubai, UAE.
+                    <div className='w-full sm:w-1/2'>
+                        <h3 className='text-center px-6 sm:px-0 sm:text-start text-[#434343] text-[10px] sm:text-lg sm:ml-8'>Backed by impeccable experience and absolute customer satisfaction, we provide all-inclusive building maintenance and renovation services in Dubai, UAE.
                         </h3>
                     </div>
                 </div>
             </div>
-            <div className='flex flex-row justify-between w-full pt-20 pb-10'>
-                <div className='w-1/2'>
+            <div className='flex flex-col sm:flex-row justify-between w-full pt-4 sm:pt-20 sm:pb-10'>
+                <div className='w-full sm:w-1/2 p-4 sm:p-0'>
                     <Image src={AboutOne} alt={'People Doing Repair work'} />
                 </div>
-                <div className='w-1/2'>
+                <div className='w-full sm:w-1/2'>
                     <Servicecard
                         imageSrc={JustOne}
                         title="Experienced Professionals"
@@ -51,19 +51,19 @@ const About: React.FC = () => {
                         title="All-In-One Service"
                         subtitle="Pellentesque id nibh tortor id. Quis vel eros donec ac odio tempor orci. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam."
                     />
-                    <div className='flex flex-row items-center p-6'>
-                        <div className='pr-4'>
-                            <Button className="capitalize rounded-full mr-4 w-fit px-8 py-2 bg-[#222222] font-semibold text-[#F9F9F9] shadow-2xl">
+                    <div className='flex flex-row items-center p-4 sm:p-6'>
+                        <div className='pr-4 w-5/12 sm:w-4/12'>
+                            <Button className="capitalize rounded-full mr-4 w-full sm:w-fit px-2 text-xs py-2 sm:text-sm sm:px-8 bg-[#222222] font-semibold text-[#F9F9F9] shadow-2xl">
                                 Read More
                             </Button>
                         </div>
-                        <div className='flex flex-row items-center'>
+                        <div className='flex flex-row items-center w-7/12 sm:w-8/12'>
                             <div className='pr-2'>
                                 <Image src={PhoneSVG} alt='phone icon'/>
                             </div>
                             <div>
-                                <h2 className='text-xl text-[#222222]'>+971 557736634</h2>
-                                <p className='text-sm text-[#434343]'>Call For Booking</p>
+                                <h2 className='text-base sm:text-xl text-[#222222]'>+971 557736634</h2>
+                                <p className='text-xs sm:text-sm text-[#434343]'>Call For Booking</p>
                             </div>
                         </div>
                     </div>
--- components/Servicecard.tsx
+++ components/Servicecard.tsx
@@ -10,13 +10,13 @@ interface ServicecardProps {
 const Servicecard: React.FC<ServicecardProps> = ({ imageSrc, title, subtitle }) => {
     return (
         <div className="flex items-center space-x-4 p-4 bg-white shadow-none ">
-            <div className="relative p-4 bg-[#EDEDED] rounded-full">
+            <div className="relative p-2 sm:p-4 bg-[#EDEDED] rounded-full">
                 <Image src={imageSrc} alt={title} layout="fixed"  width={96} height={96} />
             </div>
 
             <div>
-                <h2 className="text-xl text-[#222222] font-semibold">{title}</h2>
-                <p className="text-[#434343] text-sm">{subtitle}</p>
+                <h2 className="text-base sm:text-xl text-[#222222] font-semibold">{title}</h2>
+                <p className="text-[#434343] text-xs sm:text-sm">{subtitle}</p>
             </div>
         </div>
     );
